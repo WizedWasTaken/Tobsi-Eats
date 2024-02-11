c4c31663c0059c60c2884191b33a1bc13f3d2134
WizedWasTaken <jegerfattig1@gmail.com>
2024-02-11 23:18:25 +0100

    Remove unnecessary animation styles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,30 +12,10 @@ import NavigationComponent from '@/components/ui/Sidebar/NavigationComponent.vue
     </header>
 
     <div class="flex flex-grow p-4 mt-14 sm:ml-64">
-      <Transition name="slide-fade" mode="out-in">
-        <RouterView />
-      </Transition>
+      <RouterView />
     </div>
   </div>
 </template>
 
 <!-- Animation for page changing. -->
-<style lang="scss">
-.slide-fade-enter-active,
-.slide-fade-leave-active {
-  transition: all 0.5s ease;
-}
-.slide-fade-enter-from,
-.slide-fade-leave-to {
-  opacity: 0;
-  transform: translateX(100%);
-}
-.slide-fade-leave-active {
-  position: absolute;
-}
-.slide-fade-enter-to,
-.slide-fade-leave-from {
-  opacity: 1;
-  transform: translateX(0);
-}
-</style>
+<style lang="scss"></style>
